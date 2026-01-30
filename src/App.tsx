ededb3493b1ef87ceb66ee66fe8f3eddf46905f2
Marcus Couto <marcusvinicius-couto@hotmail.com>
2026-01-30 14:33:13 -0300

    feat: adiciona componente de header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/header"
+
 function App() {
 
   return (
     <>
-    <p className="text-error font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas natus beatae dignissimos sapiente impedit recusandae nam ut ea eaque at doloremque sit aperiam corporis sed laborum, corrupti optio soluta mollitia?</p>
+      <Header />
     </>
   )
 }
