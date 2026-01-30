f6011e99fa54f6e8c8152358c18ae1cbb3030471
Marcus Couto <marcusvinicius-couto@hotmail.com>
2026-01-30 13:23:17 -0300

    feat: adiciona fonte e cores no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
 
   return (
     <>
+    <p className="text-error font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas natus beatae dignissimos sapiente impedit recusandae nam ut ea eaque at doloremque sit aperiam corporis sed laborum, corrupti optio soluta mollitia?</p>
     </>
   )
 }
