7cef294e56711bd1691038e2316c1380352c168a
Marcus Couto <marcusvinicius-couto@hotmail.com>
2026-02-06 16:04:22 -0300

    fix: corrigido bugs no deploy

--- src/components/MenuMobile/index.tsx
+++ src/components/MenuMobile/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "@tanstack/react-router";
 import IconMenu from "@/assets/img/icon-menu.png";
 import { FaRegUserCircle } from "react-icons/fa";
-import type { NavLink } from "../Header";
+import type { NavLink } from "../Header/index";
 import { IoMdClose } from "react-icons/io";
 
 interface MenuMobileProps {
--- src/pages/_app/layout.tsx
+++ src/pages/_app/layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, createFileRoute } from "@tanstack/react-router";
-import { Header } from "../../components/Header";
+import { Header } from "../../components/Header/index";
 import { Footer } from "../../components/Footer";
 
 
