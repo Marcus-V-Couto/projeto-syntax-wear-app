e5ca17c8df78886789c18c958efdfbd302f02ff4
Marcus Couto <marcusvinicius-couto@hotmail.com>
2026-02-06 22:04:16 -0300

    .

--- src/pages/_app/layout.tsx
+++ src/pages/_app/layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, createFileRoute } from "@tanstack/react-router";
-import { Header } from "../../components/Headera";
+import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 
 export const Route = createFileRoute("/_app")({
